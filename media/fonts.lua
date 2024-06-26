8fdfc78de8e621debf6d6732c5aa71bb70dd99e0
Kate Lance <kldev2355@gmail.com>
2024-06-26 16:26:17 +0200

    adjusted tab space

--- media/fonts.lua
+++ media/fonts.lua
@@ -4,7 +4,7 @@ local _, caelUI = ...
 
 local originalFonts = {
 	NORMAL					=	[[Interface\Addons\caelUI\media\fonts\ParaType - Magistral Medium.ttf]],
-	BOLD						=	[[Interface\Addons\caelUI\media\fonts\ParaType - Magistral Bold.ttf]],
+	BOLD					=	[[Interface\Addons\caelUI\media\fonts\ParaType - Magistral Bold.ttf]],
 	BOLDITALIC				=	[[Interface\Addons\caelUI\media\fonts\ParaType - Magistral Bold Italic.ttf]],
 	ITALIC					=	[[Interface\Addons\caelUI\media\fonts\ParaType - Magistral Medium Italic.ttf]],
 	NUMBER					=	[[Interface\Addons\caelUI\media\fonts\russel square lt.ttf]],
@@ -12,12 +12,12 @@ local originalFonts = {
 	UNIT_NAME_FONT			=	[[Interface\Addons\caelUI\media\fonts\ParaType - Magistral Medium.ttf]],
 	NAMEPLATE_FONT			=	[[Interface\Addons\caelUI\media\fonts\ParaType - Magistral Medium.ttf]],
 	DAMAGE_TEXT_FONT		=	[[Interface\Addons\caelUI\media\fonts\BlankSerif.ttf]],
-	STANDARD_TEXT_FONT	=	[[Interface\Addons\caelUI\media\fonts\BlankSerif.ttf]],
+	STANDARD_TEXT_FONT		=	[[Interface\Addons\caelUI\media\fonts\BlankSerif.ttf]],
 
 	-- Addon related stuff.
-	INTERNATIONAL_FONT	=	[[Interface\Addons\caelUI\media\fonts\ParaType - Magistral Medium.ttf]],
+	INTERNATIONAL_FONT		=	[[Interface\Addons\caelUI\media\fonts\ParaType - Magistral Medium.ttf]],
 	CUSTOM_NUMBERFONT		=	[[Interface\Addons\caelUI\media\fonts\russel square lt.ttf]],
-	SCROLLFRAME_NORMAL	=	[[Interface\Addons\caelUI\media\fonts\neuropol nova cd rg.ttf]],
+	SCROLLFRAME_NORMAL		=	[[Interface\Addons\caelUI\media\fonts\neuropol nova cd rg.ttf]],
 	SCROLLFRAME_BOLD		=	[[Interface\Addons\caelUI\media\fonts\neuropol nova cd bd.ttf]],
 	ADDON_FONT				=	[[Interface\Addons\caelUI\media\fonts\ParaType - Magistral Medium.ttf]],
 	NAMEPLATE_FONT			=	[[Interface\Addons\caelUI\media\fonts\ParaType - Magistral Medium.ttf]],
